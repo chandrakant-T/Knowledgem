4f0cbe824d9ed8b44d7de424123dab393eb3725f
chandrakant-T <trivedichandrakant559@gmail.com>
2026-01-28 15:11:55 +0530

    forget-pass error

--- Backend/Config/mailer.js
+++ Backend/Config/mailer.js
@@ -1,11 +1,24 @@
 const nodemailer = require("nodemailer");
 
+// const transporter = nodemailer.createTransport({
+//   service: "gmail",
+//   auth: {
+//     user: process.env.EMAIL,
+//     pass: process.env.EMAIL_PASS,
+//   },
+// });
+
+
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL,
     pass: process.env.EMAIL_PASS,
   },
+  connectionTimeout: 10000, // 10 seconds
 });
 
 module.exports = transporter;
